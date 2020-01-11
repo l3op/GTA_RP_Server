77cf4a0fc02853f0ef0078d411fff3ab4f8b13d8
YiShan8787 <59713684gx@gmail.com>
2020-01-11 15:07:45 +0800

    translate

---
+++ server-data/resources/[esx]/esx_joblisting/locales/tc.lua
@@ -0,0 +1,5 @@
+Locales['tc'] = {
+	['new_job'] = '你獲得了一份新工作！',
+	['access_job_center'] = '按 ~INPUT_PICKUP~ 打開~b~工作列表~s~.',
+	['job_center'] = '招聘中心',
+}
---
+++ server-data/resources/[esx]/esx_skin/locales/tc.lua
@@ -0,0 +1,6 @@
+Locales['tc'] = {
+  ['skin_menu'] = '皮膚選單',
+  ['use_rotate_view'] = '使用 ~INPUT_VEH_FLY_ROLL_LEFT_ONLY~ 和 ~INPUT_VEH_FLY_ROLL_RIGHT_ONLY~ 旋轉視圖',
+  ['skin'] = '更改皮膚',
+  ['saveskin'] = '保存',
+}
