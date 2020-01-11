ecd060429032e92d70316c3062b42a9c129719ad
YiShan8787 <59713684gx@gmail.com>
2020-01-11 16:13:07 +0800

    translate

---
+++ server-data/resources/[esx]/esx_atm/locales/tc.lua
@@ -0,0 +1,7 @@
+Locales['tc'] = {
+  ['invalid_amount'] = '~r~無效金額~s~',
+  ['deposit_money']  = '妳存入了~g~$%s~s~',
+  ['withdraw_money'] = '妳取走了~g~$%s~s~',
+  ['press_e_atm']    = '按 ~INPUT_PICKUP~ 使用 ~g~ATM~s~',
+  ['atm_blip']       = 'ATM',
+}
---
+++ server-data/resources/[esx]/esx_basicneeds/locales/tc.lua
@@ -0,0 +1,4 @@
+Locales['tc'] = {
+	['used_bread'] = '你吃了~y~一塊~s~~b~麵包~s~',
+	['used_water'] = '你喝了~y~一瓶~s~~b~礦泉水~s~',
+}
