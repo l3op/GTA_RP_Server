b61aeff81ef70e4c7cc2afc088ca10abf2b022cd
YiShan8787 <59713684gx@gmail.com>
2020-01-11 14:55:06 +0800

    billing translate

---
+++ server-data/resources/[esx]/esx_billing/locales/tc.lua
@@ -0,0 +1,10 @@
+Locales['tc'] = {
+  ['invoices'] = '賬單',
+  ['invoices_item'] = '$%s',
+  ['received_invoice'] = '妳剛剛~r~收到了~s~壹份賬單',
+  ['paid_invoice'] = '妳~g~支付了~s~壹份~r~$%s~s~的賬單',
+  ['received_payment'] = '妳~g~收到了~s~~r~$%s~s~的付款',
+  ['player_not_online'] = '玩家不在線',
+  ['no_money'] = '妳沒有足夠的錢來支付這筆賬單',
+  ['target_no_money'] = '玩家~r~沒有~s~足夠的錢支付這個賬單!',
+}
