c0970dfc7e5c1e5e85cea28850d80506685eeb9a
YiShan8787 <59713684gx@gmail.com>
2020-01-12 02:19:37 +0800

    mod

---
+++ server-data/resources/[esx]/eden_demarches/README.md
@@ -0,0 +1,32 @@
+# eden_demarches
+
+Menu avec 37 démarches differentes pour convenir à chaques situation.
+
+ -- Si quelqu'un est interesser pour faire une traduction anglaise envoyez moi un message privé sur discord: Solaris#6632
+ 
+Menu that changes how your character moves. 
+
+ -- If someone is interest about make an english translation PM me on discord : Solaris#6632
+ 
+[INSTALLATION]
+
+1) CD in your resources/[esx] folder
+2) Clone the repository
+```
+git clone https://github.com/ShadowsX2V2/eden_demarches.git
+```
+3) Add this in your server.cfg :
+
+```
+start eden_demarches
+```
+ 
+
+### SCREENS
+
+https://image.noelshack.com/fichiers/2017/47/3/1511375483-capture-d-ecran-2017-11-14-15-05-09.png
+https://image.noelshack.com/fichiers/2017/47/3/1511375480-capture-d-ecran-2017-11-14-15-04-54.png
+
+
+
+
---
+++ server-data/resources/[esx]/eden_demarches/locales/en.lua
@@ -0,0 +1,73 @@
+Locales['en'] = {
+
+
+	--Title and subtitles 
+	
+		['demarches'] = 'Walk',
+		['categories'] = 'Categories',
+		['shumeurs'] = 'Moods',
+		['sattitudes'] = 'Attitudes',
+		['sautres'] = 'Other',
+		['sfemmes'] = 'Random',
+		['reset'] = '~r~Reset',
+		['quitter'] = "~o~Quit",
+		
+	--Main Menu
+	
+		['humeurs'] = '~b~Moods',
+		['attitudes'] = '~b~Attitudes',
+		['autres'] = '~b~Other',
+		['femmes'] = '~b~Women',
+	
+	-- Moods
+	
+		['determine'] = 'Determined',
+		['triste'] = 'Sad',
+		['depression'] = 'Very Sad',
+		['enerve'] = 'Annoyed',
+		['presse'] = 'Eager',
+		['timide'] = 'Shy',
+		['lunatique'] = 'Moody',
+		['stresse'] = 'Stressed',
+		['flemme'] = 'Lazy',
+		
+	-- Attitudes
+	
+		['hautain'] = 'Proud',
+		['badboy'] = 'Bad Boy',
+		['gangster'] = 'Gangster',
+		['fraquasse'] = 'Cold',
+		['coquille'] = 'Empty Shell',
+		['coince'] = 'Stuck Up',
+		['perdu'] = 'Lost',
+		['intimidant'] = 'Intimidating',
+		['richissime'] = 'Rich',
+		['hargneux'] = 'Aggressive',
+		['imposant'] = 'Imposing',
+		['frimeur'] = 'Show-off',
+		
+	-- Others
+	
+		['randonneur'] = 'Backpacker',		
+		['blesse'] = 'Injured',
+		['obese'] = 'Fat',			
+		['fesses'] = 'Butt',
+		['detente'] = 'Relaxed',
+		
+	-- Special Women
+	
+		['arrogante'] = 'Arrogant',
+		['classique'] = 'Classical',
+		['fragile'] = 'Fragile',
+		['enerve'] = 'Angry',
+		['fatale'] = 'Fatal',
+		['fuite'] = 'Flight',
+		['tristesse'] = 'Sad',
+		['rebelle'] = 'Rebel',
+		['serieuse'] = 'Serious',
+		['ffesses'] = 'Roll Butt',
+		['hautaines'] = 'Proud',
+}
+
+
+
