daadaf17d0a6c221df45b79f96e438cd21b4011c
YiShan8787 <59713684gx@gmail.com>
2020-01-11 18:44:47 +0800

    translate

---
+++ server-data/resources/[esx]/esx_holdup/locales/tc.lua
@@ -0,0 +1,16 @@
+Locales['tc'] = {
+  ['shop_robbery'] = '超商搶劫',
+  ['press_to_rob'] = '按下 ~INPUT_CONTEXT~ 來 ~o~rob~s~ ~b~%s~s~',
+  ['robbery_timer'] = '超商搶劫時間: ~r~%s~s~ 天',
+  ['recently_robbed'] = '這個超商最近被搶了。 請等待 ~y~%s~s~ 天， 直到你可以再次搶劫',
+  ['rob_in_prog'] = '~r~robbery in progress at ~b~%s~s~',
+  ['started_to_rob'] = '你開始搶劫 ~y~%s~s~',
+  ['alarm_triggered'] = '警報已經觸發',
+  ['robbery_complete'] = '~r~搶劫成功~s~, 你 ~o~搶了~s~ ~g~$%s~s~',
+  ['robbery_complete_at'] = '~r~搶劫發生在 ~y~%s~s~',
+  ['robbery_cancelled'] = '搶劫取消!',
+  ['robbery_cancelled_at'] = '~r~發生在 ~b~%s~s~ 的搶劫，已經被取消!',
+  ['min_police'] = '這裡至少必須有 ~b~%s 警察~s~ 在城裡，才能搶劫.',
+  ['robbery_already'] = '~r~搶劫正在發生中。',
+  ['no_threat'] = '你對店主不構成威脅',
+}
--- server-data/resources/[esx]/esx_rpchat/config.lua
+++ server-data/resources/[esx]/esx_rpchat/config.lua
@@ -1,5 +1,5 @@
 Config        = {}
-Config.Locale = 'en'
+Config.Locale = 'tc'
 
 Config.EnableESXIdentity = false -- only turn this on if you are using esx_identity and want to use RP names
 Config.OnlyFirstname     = false
