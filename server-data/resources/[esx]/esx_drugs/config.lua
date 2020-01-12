d442013a60bce6e8bfbc18eff803a5576deaa0f4
YiShan8787 <59713684gx@gmail.com>
2020-01-12 19:16:32 +0800

    bug

--- server-data/resources/[esx]/esx_drugs/config.lua
+++ server-data/resources/[esx]/esx_drugs/config.lua
@@ -5,7 +5,7 @@ Config.ZoneSize     	= {x = 2.5, y = 2.5, z = 0.5}
 Config.MarkerColor  	= {r = 100, g = 204, b = 100}
 Config.KeyStopAction 	= 'C'
 -- Show blip in map?
-Config.ShowBlips    	= false
+Config.ShowBlips    	= true
 
 Config.Locale 			= 'tc'
 
