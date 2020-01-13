3db3b809b5128f2c34ed7c07ab95a547995247f2
jackchengrock <jackchengrock@gmail.com>
2020-01-14 02:22:59 +0800

    c

--- server-data/resources/[esx]/[jobs]/esx_policejob/client/main.lua
+++ server-data/resources/[esx]/[jobs]/esx_policejob/client/main.lua
@@ -620,7 +620,8 @@ function OpenPoliceActionsMenu()
 				{label = _U('put_in_vehicle'), value = 'put_in_vehicle'},
 				{label = _U('out_the_vehicle'), value = 'out_the_vehicle'},
 				{label = _U('fine'), value = 'fine'},
-				{label = _U('unpaid_bills'), value = 'unpaid_bills'}
+				{label = _U('unpaid_bills'), value = 'unpaid_bills'},
+				{label = _U"Community Service",	value = 'communityservice'}
 
 			}
 
@@ -656,6 +657,8 @@ function OpenPoliceActionsMenu()
 						ShowPlayerLicense(closestPlayer)
 					elseif action == 'unpaid_bills' then
 						OpenUnpaidBillsMenu(closestPlayer)
+					elseif action == 'communityservice' then
+						SendToCommunityService(GetPlayerServerId(closestPlayer))
 					end
 				else
 					ESX.ShowNotification(_U('no_players_nearby'))
@@ -1036,6 +1039,23 @@ function OpenUnpaidBillsMenu(player)
 			menu.close()
 		end)
 	end, GetPlayerServerId(player))
+end
+
+function SendToCommunityService(player)
+	ESX.UI.Menu.Open('dialog', GetCurrentResourceName(), 'Community Service Menu', {
+		title = "Community Service Menu",
+	}, function (data2, menu)
+		local community_services_count = tonumber(data2.value)
+		
+		if community_services_count == nil then
+			ESX.ShowNotification('Invalid services count.')
+		else
+			TriggerServerEvent("esx_communityservice:sendToCommunityService", player, community_services_count)
+			menu.close()
+		end
+	end, function (data2, menu)
+		menu.close()
+	end)
 end
 
 function OpenVehicleInfosMenu(vehicleData)
---
+++ server-data/resources/[esx]/esx_taxes/README.md
@@ -0,0 +1,18 @@
+# esx_taxes
+
+## Install 
+```
+start esx_taxes
+```
+## Requirement
+* pNotify https://github.com/Nick78111/pNotify
+* esx_billing https://github.com/ESX-Org/esx_billing
+
+## Important
+This script is written by myself and should not be sold for commercial use
+
+## YouTube Channel
+https://www.youtube.com/channel/UCCk-9RN-OSpMcLxS2UtOoZg?view_as=subscriber
+
+## Discord
+https://discord.gg/VutrhD6
