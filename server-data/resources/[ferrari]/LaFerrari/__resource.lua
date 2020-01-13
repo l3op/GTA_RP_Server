fd3121bc51c521dbe1a01b188aecefa0ce037dcb
HoKuanYu <40305418+HoKuanYu@users.noreply.github.com>
2020-01-13 20:37:25 +0800

    lafa and cali

--- server-data/resources/[ferrari]/LaFerrari/__resource.lua
+++ server-data/resources/[ferrari]/LaFerrari/__resource.lua
@@ -8,7 +8,7 @@ files {
     'data/carcols.meta',
     'data/handling.meta',
 --    'data/vehiclelayouts.meta',
-    'data/contentunlocks.meta',
+--    'data/contentunlocks.meta',
 }
 
 data_file 'HANDLING_FILE' 'data/handling.meta'
@@ -17,6 +17,6 @@ data_file 'CARCOLS_FILE' 'data/carcols.meta'
 data_file 'VEHICLE_VARIATION_FILE' 'data/carvariations.meta'
 --data_file 'VEHICLE_LAYOUTS_FILE''data/vehiclelayouts.meta'
 --data_file 'VEHICLE_SHOP_DLC_FILE' 'data/'
-data_file 'CONTENT_UNLOCKING_META_FILE' 'data/contentunlocks.meta'
+--data_file 'CONTENT_UNLOCKING_META_FILE' 'data/contentunlocks.meta'
 
 --client_script { 'data/vehicle_names.lua' }
