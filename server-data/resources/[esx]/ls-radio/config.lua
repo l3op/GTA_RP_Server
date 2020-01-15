fa9dc23bcc21101a00a935feee575944e15a2224
jackchengrock <jackchengrock@gmail.com>
2020-01-15 13:33:24 +0800

    Update config.lua

--- server-data/resources/[esx]/ls-radio/config.lua
+++ server-data/resources/[esx]/ls-radio/config.lua
@@ -1,7 +1,7 @@
 Config = {}
 
 Config.RestrictedChannels = 10 -- channels that are encrypted (EMS, Fire and police can be included there) if we give eg 10, channels from 1 - 10 will be encrypted
-Config.enableCmd = false --  /radio command should be active or not (if not you have to carry the item "radio") true / false
+Config.enableCmd = true --  /radio command should be active or not (if not you have to carry the item "radio") true / false
 
 Config.messages = {
 
