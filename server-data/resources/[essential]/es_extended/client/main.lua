81e99a665ea23d80c275d9a41d82f869f486f228
HoKuanYu <40305418+HoKuanYu@users.noreply.github.com>
2020-01-16 04:10:46 +0800

    tp

--- server-data/resources/[essential]/es_extended/client/main.lua
+++ server-data/resources/[essential]/es_extended/client/main.lua
@@ -233,7 +233,7 @@ AddEventHandler('esx:teleport', function(pos)
 	pos.y = pos.y + 0.0
 	pos.z = pos.z + 0.0
 
-	print(GetActivePlayers())
+	print(table.tostring(GetActivePlayers())
 
 	RequestCollisionAtCoord(pos.x, pos.y, pos.z)
 
